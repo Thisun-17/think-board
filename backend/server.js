f6a5aa689bdb081cfed10caa7037d9ef1d1ddf07
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-08-09 23:49:23 +0530

    Refactor server.js: add comments for clarity and improve code readability

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
 import express from 'express'
+// Import the express module
 
 const app = express();
+// Create an instance of an Express application
 
 app.get("/api/notes", (req, res) => {
   res.send('your notes here');
-}); 
+}); // Endpoint to get notes 
+
+app.post("/api/notes", (req, res) => {
+  res.send('note added');
+}); // Endpoint to add a note 
+
 
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
-});
+}); // Start the server on port 3000 
