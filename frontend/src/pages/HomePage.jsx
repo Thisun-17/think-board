0a63fe1e1dddcf553689d755141d6651d0419826
Thisun-17 <thisun.20233173@iit.ac.lk>
2025-08-13 16:32:27 +0530

    feat: implement Navbar component and integrate with HomePage for navigation

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'  
+import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI';
+import { useEffect } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes, setNotes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await axios.get('http://localhost:3000/api/notes');
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log('Failed to fetch notes:');
+        console.error(error); 
+        if (error.response?.status === 429) {
+          setIsRateLimited(true);
+        } else {
+          toast.error('Failed to fetch notes. Please try again later.');
+        }
+      } finally {
+        setLoading(false);
+      } 
+    };
+
+    fetchNotes();
+  }, []);  
+
+
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}  
+    </div>
   )
 }
 
